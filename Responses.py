425f50b24c9b9523712798403b86940e2bd5c8b7
Asaf Ben Shabat <asafbs177@gmail.com>
2021-03-23 03:58:36 +0200

    bot_update

--- Responses.py
+++ Responses.py
@@ -2,16 +2,20 @@ from datetime import datetime
 
 
 def sample_responses(input_text):
-    user_messages = str(input_text).lower()
-    if user_messages in ("שלום"):
-        return  "היי שלום מה קורה ?"
+    # response handling "input text" is the user massage
+    user_messages = str(input_text)
+    if user_messages in ("שלום", "?", "היי"):
+        return "היי שלום"
 
-    if user_messages in ("מי אתה ?","מי אתה?"):
-        return  "אני הבוט של תל חי"
-
-    if user_messages in ("שעה?","תאריך?"):
+    if user_messages in ("מי אתה ?", "מי אתה?"):
+        return "אני הבוט של תל חי"
+    if user_messages in ("מה קורה ?", "מה המצב?"):
+        return "אני בסדר איך אוכל לעזור?"
+    if user_messages in ("שעה?", "תאריך?"):
         now = datetime.now()
         date_time = now.strftime("%d/%m/%y, %H:%M:%S")
-
         return str(date_time)
-    return "I dont understand you ! try again!"
+    if user_messages in("מי יצר אותך?"):
+        return  "@ChesireIL"
+
+    return "ממממ סורי לא הבנתי אותך תנסה להיות יותר ספציפי"
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import Constans as keys
 from telegram.ext import *
 import Responses as R
 
-print("the bot is online!")
-
 
 def start_command(update, context):
     update.message.reply_text('היי אני הבוט של תל חי ואני כאן כדי לעזור')
@@ -24,17 +22,25 @@ def error(update, context):
     print(f"Update {update} caused an error {context.error}")
 
 
+def update_msg():
+    pass
+
+
 def main():
-    updater = Updater(keys.API_KEY, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
-
-    dp.add_handler(MessageHandler(Filters.text, handle_message))
-    dp.add_error_handler(error)
-    updater.start_polling()
-    updater.idle()
+    cmd = input("choose a command for the bot (start,edit,exit): ")
+    if cmd.lower() == "start" or cmd.lower().startswith("s"):
+        updater = Updater(keys.API_KEY, use_context=True)
+        dp = updater.dispatcher
+        # dp.add_handler(CommandHandler("start", start_command))
+        dp.add_handler(CommandHandler("help", help_command))
+        dp.add_handler(MessageHandler(Filters.text, handle_message))
+        dp.add_error_handler(error)
+        updater.start_polling()
+        updater.idle()
+    if cmd.lower() == "edit" or cmd.lower().startswith("e"):
+        pass
+    else:
+        exit()
 
 
 main()
